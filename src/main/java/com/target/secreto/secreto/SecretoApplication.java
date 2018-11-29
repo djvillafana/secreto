5a60e65a953a01846efad59172e45c921a138a4b
David.Villafana <david.villafana@target.com>
2018-11-29 15:27:18 -0600

    initial commit

---
+++ src/main/java/com/target/secreto/secreto/SecretoApplication.java
@@ -0,0 +1,12 @@
+package com.target.secreto.secreto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretoApplication.class, args);
+	}
+}
